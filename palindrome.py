387c49ab667ce8bed277f3653cd4d01ad4091f21
Alex Warren <WarrenWorkshopMedia@gmail.com>
2025-03-28 23:53:49 -0400

    Palindromes Completed

--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,25 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+
+#Open words.txt file. Lets get this started.
+PalindromeFile = open("words.txt")
+
+#Read all lines and split information into separate lines.
+ListedWords = PalindromeFile.read().splitlines()
+
+#Close the file, no need to crash the system
+PalindromeFile.close()
+
+#Initialize palindrome counter to zero since its good practice.
+PalindromeCounter = 0
+
+#Loop and Look through the words and check for words that are palindromes.
+for Word in ListedWords:
+    #Check to see if the word is the same if reversed and add one to counter if true.
+    if Word == Word[::-1]:
+        PalindromeCounter += 1
+
+#Display the total amount of palindromes in the file. Quest Completed. +100 Gold
+print(f"The number of palindromes in the file is {PalindromeCounter}")
